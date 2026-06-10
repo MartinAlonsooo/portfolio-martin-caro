3a99125921ae63dc3605f51face0e3843ef53445
Martin Caro <mcaroberrios@gmail.com>
2026-06-09 21:16:11 -0400

    Conectar frontend con backend Render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://portfolio-martin-caro.onrender.com/api/"
+  baseURL: "https://portfolio-martin-caro.onrender.com/api/"
 });
 
 export default api;
